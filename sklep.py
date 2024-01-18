19fed01f1cbb302fe67db41c3582369eef4913a5
Mateusz <mateusz.gurgul.2003@gmail.com>
2024-01-18 20:34:27 +0100

    Add text with number of all products in shops

--- sklep.py
+++ sklep.py
@@ -6,3 +6,7 @@ shoppingList = {
 for shop, item in shoppingList.items():
     shop = shop.capitalize()
     print(f"Ide do {shop} i kupie tam {item}")
+
+    allItems = len(item) + len(item)
+
+print(f"W sumie kupuję {allItems} produktów")
